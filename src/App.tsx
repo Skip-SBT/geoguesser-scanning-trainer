c6a75e38b44ca9ec5edb2d409d3244800e196e2e
mhubrath <maximilian.hubrath@gmail.com>
2026-05-01 21:24:40 +0200

    Replace BrowserRouter with HashRouter for improved routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import TimedGamePage from './pages/TimedGamePage';
 import theme from './theme';
 import {ThemeProvider} from '@mui/material';
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/trainer" element={<ScanningTrainerPage />} />
                     <Route path="/game" element={<TimedGamePage />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </ThemeProvider>
     );
 }
